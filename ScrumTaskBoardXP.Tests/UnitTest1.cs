0e8b878da782a1e895e0de31e7b636ab58b70695
SercanBayrambey <sercanbayrambeyy@gmail.com>
2020-12-31 16:22:36 +0300

    ADDED: Test Layer

---
+++ ScrumTaskBoardXP.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrumTaskBoardXP.Business.Abstract;
+using ScrumTaskBoardXP.Business.Concrete;
+using ScrumTaskBoardXP.Data.Concrete;
+using ScrumTaskBoardXP.Web.Controllers;
+using System.Threading.Tasks;
+
+namespace ScrumTaskBoardXP.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+
+        }
+    }
+}
